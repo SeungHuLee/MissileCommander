d30a0ccad63df49efe3f521bcd777e53c93ca2f9
SEUNGHU LEE <gadlsh1208@naver.com>
2019-11-29 18:30:06 +0900

    [Update] Add bullet launch delay

--- MissileCommander/Assets/_Scripts/BulletLauncher.cs
+++ MissileCommander/Assets/_Scripts/BulletLauncher.cs
@@ -7,6 +7,9 @@ namespace MissileCommander
     {
         [SerializeField] private Bullet bulletPrefab;
         [SerializeField] private Transform firePosition;
+        [SerializeField] private float fireDelay = 0.5f;
+        private float _elapsedFireTime;
+        private bool _canShoot = true;
 
         private Factory _bulletFactory;
 
@@ -15,12 +18,29 @@ namespace MissileCommander
             _bulletFactory = new Factory(bulletPrefab);
         }
 
+        private void Update()
+        {
+            if (!_canShoot)
+            {
+                _elapsedFireTime += Time.deltaTime;
+                if (_elapsedFireTime >= fireDelay)
+                {
+                    _canShoot = true;
+                    _elapsedFireTime = 0f;
+                }
+            }
+        }
+
         public void OnFireButtonPressed(Vector3 mousePosition)
         {
+            if (!_canShoot) { return; }
+            
             // Instantiate Bullet
             Bullet bullet = _bulletFactory.Get() as Bullet;
             bullet.Activate(firePosition.position, mousePosition);
             bullet.OnDestroyed += OnBulletDestroyed;
+
+            _canShoot = false;
         }
 
         public void OnBulletDestroyed(Bullet usedBullet)
